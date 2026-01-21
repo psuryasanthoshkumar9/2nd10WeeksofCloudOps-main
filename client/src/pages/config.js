d7bbf7647281e7fada6f36486f3d96a307ddefa0
P Surya Santhosh Kumar <123743514+psuryasanthoshkumar9@users.noreply.github.com>
2026-01-21 21:41:07 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://13.223.200.36:84"
+const API_BASE_URL = "a64f87efaab2b4cc1991e12e804f8ea6-409118344.us-west-2.elb.amazonaws.com"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
